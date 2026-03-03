9bdb2359f5a1239ccd52f5c8fc7e0d17b84814a0
jenspromess <jenspromess@web.de>
2026-03-03 22:32:44 +0100

    added role selection

--- main.js
+++ main.js
@@ -3,16 +3,54 @@ document.addEventListener("DOMContentLoaded", () => {
     // alle Werewolf Cards: https://boardgamegeek.com/thread/2111933/all-roles-tile-images
 
     const roleGrid = document.querySelector(".roles-grid");
-    const roles = ["Werewolf", "Werewolf", "Seer", "Villager", "Villager", "Villager"];
+    const roles = [
+        {
+            "id": "werewolf1",
+            "name": "Werewolf"
+        },
+        {
+            "id": "werewolf2",
+            "name": "Werewolf"
+        },
+        {
+            "id": "seer",
+            "name": "Seer"
+        },
+        {
+            "id": "villager1",
+            "name": "Villager"
+        },
+        {
+            "id": "villager2",
+            "name": "Villager"
+        },
+        {
+            "id": "villager3",
+            "name": "Villager"
+        }
+    ]
+    let activatedRoles = [];
 
     for (const role of roles) {
         const div = document.createElement("div");
         div.classList.add("role-card");
         const img = document.createElement("img");
-        img.src = "./images/" + role.toLowerCase() + ".png";
-        img.alt = role;
+        img.src = "./images/" + role.name.toLowerCase() + ".png";
+        img.alt = role.name;
 
         div.append(img);
         roleGrid.append(div);
+
+        div.addEventListener("click", () => {
+            console.log(div.style.border);
+            if (!div.style.border || div.style.border === "none") {
+                div.style.border = "white 5px solid";
+                activatedRoles.push(role);
+            } else {
+                div.style.border = "none";
+                activatedRoles = activatedRoles.filter(role1 => role1 !== role);
+            }
+            console.log(activatedRoles);
+        });
     }
 });
